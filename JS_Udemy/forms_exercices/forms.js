bcb8e881d8a83bcd86d79858aded89f878419a24
ezitounioussama <ezitounioussama@gmail.com>
2022-08-21 13:43:29 +0100

    formsClickEv

--- JS_Udemy/forms_exercices/forms.js
+++ JS_Udemy/forms_exercices/forms.js
@@ -6,3 +6,32 @@ document.getElementById("clickMe").onclick = function () {
   let selectedValue = selectField.options[selectOption].innerHTML;
   document.getElementById("result").innerHTML = selectedValue;
 };
+
+// radioButton
+
+document.getElementById("showMe").onclick = () => {
+  let nameRadio = document.getElementsByName("gender");
+
+  let radioSel;
+
+  for (let i = 0; i < nameRadio.length; i++) {
+    if (nameRadio[i].checked) {
+      radioSel = nameRadio[i];
+      document.getElementById("radioResult").innerHTML = radioSel.value;
+    }
+  }
+};
+
+// CheckBox
+
+document.getElementById("show").onclick = () => {
+  let nameCheck = document.getElementsByName("interest");
+  document.getElementById("checkResult").innerHTML = "";
+
+  for (let i = 0; i < nameCheck.length; i++) {
+    if (nameCheck[i].checked) {
+      document.getElementById("checkResult").innerHTML +=
+        "<li>" + nameCheck[i].value + "</li>";
+    }
+  }
+};
