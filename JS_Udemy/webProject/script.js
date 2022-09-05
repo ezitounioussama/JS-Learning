28daa55c2e8de06048c9f3188ebe8d8850a2a14e
ezitounioussama <zitounioussama95@gmail.com>
2022-09-05 16:53:30 +0100

    about us done

--- JS_Udemy/webProject/script.js
+++ JS_Udemy/webProject/script.js
@@ -38,3 +38,38 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
+//about section {BOX-TABS}
+
+let aboutUs = {
+  Mission:
+    "Duis ac leo nisi. Mauris nec ex id lorem commodo rutrum rutrum a est. Cras facilisis sit amet lectus non posuere. Nullam non magna non enim blandit elementum.",
+
+  Vision:
+    "Praesent ut lacinia neque, faucibus suscipit quam. Duis sed nunc rutrum, tempor mi at, euismod nibh.",
+
+  Values:
+    "<ul><li>Nunc iaculis</li><li>Donec dictum fringilla</li><li>Duis convallis tortor ultrices</li><li>Curabitur in est lectus</li><li>Maecenas condimentum elit</li></ul>",
+};
+
+let space = `
+`;
+
+let selectedColor = "#1F2937";
+let unselectedColor = "#eeeeee";
+
+let tabs = document.getElementsByClassName("single-tab");
+let tabBox = document.getElementById("box-text");
+for (let a = 0; a < tabs.length; a++) {
+  tabs[a].onclick = function () {
+    var clickedValue = this.innerHTML.replace(/\s/g, "");
+
+    tabBox.innerHTML = aboutUs[clickedValue];
+
+    for (let b = 0; b < tabs.length; b++) {
+      tabs[b].style["background-color"] = unselectedColor;
+      tabs[b].style["color"] = unselectedColor;
+    }
+    this.style["background-color"] = selectedColor;
+  };
+}
