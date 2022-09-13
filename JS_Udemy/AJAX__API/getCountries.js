1396d2921309d385dc14729a9d777493c7a96fd7
ezitounioussama <zitounioussama95@gmail.com>
2022-09-13 15:51:21 +0100

    vue JS

--- JS_Udemy/AJAX__API/getCountries.js
+++ JS_Udemy/AJAX__API/getCountries.js
@@ -9,6 +9,8 @@ function get_countries() {
       populate_countries(data);
       if (localStorage.selectedCountry) {
         $("#countries").val(localStorage.selectedCountry);
+        var countryName = $("#countries option:selected").text();
+        get_holidays(localStorage.selectedCountry, countryName);
       }
     },
     error: function () {
@@ -32,7 +34,47 @@ function populate_countries(countries) {
   // stock the value of the option selected in the local storage
   $("#countries").change(function () {
     localStorage.setItem("selectedCountry", $("#countries").val());
+    var countryName = $("#countries option:selected").text();
+    get_holidays($("#countries").val(), countryName);
   });
 }
 
 get_countries();
+
+// holiday API https://holidayapi.com/v1/holidays?pretty&key=206ddac1-5a07-4310-b28d-03204536d606&country=MA&year=2021
+
+function get_holidays(countryCode, countryName) {
+  $("#countryName").text("");
+  $("#year").text("");
+  $("#holidayList").html("");
+  let previousYear = new Date().getFullYear() - 1;
+  $.ajax({
+    url:
+      "https://holidayapi.com/v1/holidays?pretty&key=" +
+      "206ddac1-5a07-4310-b28d-03204536d606" +
+      "&country=" +
+      countryCode +
+      "&year=" +
+      previousYear,
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
+      let holidays = data.holidays;
+      $("#countryName").text(countryName);
+      $("#year").text(previousYear);
+      $("#holidayList").html("");
+      for (var a = 0; a < holidays.length; a++) {
+        if (holidays[a].public) {
+          var listItem = "<li>";
+          listItem += holidays[a].date + " - " + holidays[a].name;
+          listItem += "</li>";
+
+          $("#holidayList").append(listItem);
+        }
+      }
+    },
+    error: function () {
+      console.log("Error in the request");
+    },
+  });
+}
